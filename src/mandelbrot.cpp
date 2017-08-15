d20664b6230fbc8d3dbf0b366ac08e6706f4fb9f
S Leibrock <steven.leibrock@gmail.com>
2017-08-15 16:59:27 -0400

    Adding work in progress code

--- src/mandelbrot.cpp
+++ src/mandelbrot.cpp
@@ -1,124 +1,94 @@
 #include <iostream>
 #include <fstream>
 #include <cstdint>
-#include <unistd.h>
+#include <getopt.h>
 #include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <math.h>
 
 // use GMP soon for ultra precision
 //#include <gmp.h>
 
+// project level includes
+#include "utils.h"
+
 #define MAX_ITERS              255
-#define DEFAULT_WIDTH         7680
-#define DEFAULT_HEIGHT        4320
-#define MANDELBROT_THRESHOLD   4.0
 
 /*
-// program args with getopts/unistd
-static struct option long_options[5] = {
-    {"real",   1, 0,    0},
-    {"imag",   1, 0,    0},
-    {"output", 1, 0,    0},
-    {"colors", 1, 0,    0},
-    {NULL,     0, NULL, 0}
-};
-*/
-
-int main(int argc, char **argv)
+ * render a mandelbrot set based on the settings_t var
+ */
+int render(Settings* settings)
 {
-    // Command line options
-    // --width=<uint32_t>  - width of the output image
-    // --height=<uint32_t> - height of the output image
-    // --real=<double>     - starting position (x, real)
-    // --imag=<double>     - starting position (y, imag)
-    // --output=<string>   - file output name (output.ppm)
-    // --colors=<string>   - name of the color map to use
-    // --verbose           - print info for debug purposes 
-
-    // make that nice 8k res image
-    uint32_t width  = DEFAULT_WIDTH;
-    uint32_t height = DEFAULT_HEIGHT;
-
-    // write data to a file
+    // open up a file stream and allocate the file header
     std::ofstream ofs("./mandel.ppm", std::ios::out | std::ios::binary);
     ofs << "P6\n";
     ofs << "#This is a mandelbrot image\n";
-    ofs << width << " " << height << "\n255\n";
+    ofs << settings->output_res->width << " " << settings->output_res->height;
+    ofs << "\n255\n";
 
-    // use to print out more text
-    int verbose = 1;
-
-    // magnification level - increase to zoom in more, decrease for less (!=0)
-    const double magnification = 64000;
-
-    // seeds - the center of the image is (init_re, init_im)
-    const double init_re = -0.1011;
-    const double init_im = 0.9563;
-    const double inc_re  = (1.0 / magnification);
-    const double inc_im  = (1.0 / magnification);
-
-    // find the top-left x-y pair
-    const double top_x  = init_re - ((double(width)*0.5) * inc_re);
-    const double top_y = init_im - ((double(height)*0.5) * inc_im);
-
+    double inc  = 0.01;
     // constant C values
-    double c_re = top_x;
-    double c_im = top_y;
+    double c_re = settings->init_real;
+    double c_im = settings->init_imag;
 
     // z values to mutate for iteration
     double z_re, z_re2;
     double z_im, z_im2;
-
-    if(verbose)
+    
+    
+    if(settings->verbose)
     {
         std::cout << "===================================" << std::endl;
         std::cout << "|       Mandelbrot Program        |" << std::endl;
         std::cout << "===================================" << std::endl;
         std::cout << std::endl;
 
-        std::cout << "Desired point: " << init_re << ", " << init_im << std::endl;
-        std::cout << "Magnification: " << magnification << std::endl;
-        std::cout << "Top-left point: " << top_x << ", " << top_y << std::endl;
-        std::cout << "Increments: " << inc_re << ", " << inc_im << std::endl;
+        std::cout << "Target resolution: " << settings->output_res->width 
+            << "x" << settings->output_res->height << std::endl;
+        std::cout << std::endl;
+
+        //std::cout << "Desired point: " << init_re << ", " << init_im << std::endl;
+        //std::cout << "Magnification: " << magnification << std::endl;
+        //std::cout << "Top-left point: " << top_x << ", " << top_y << std::endl;
+        std::cout << "Increment: " << inc << std::endl;
 
         std::cout << std::endl;
         std::cout << "Beginning iteration ... " << std::endl;
     }
 
-    // new increment-based method
     unsigned char iter;
-    for(unsigned int y=0; y< height; y++)
+    for(unsigned int y=0; y < settings->output_res->height; y++)
     {
-        for(unsigned int x=0; x < width; x++)
+        for(unsigned int x=0; x < settings->output_res->width; x++)
         {
-            // reset the values for the x-loop
-            iter  = 0;
-            z_re  = 0.0;
-            z_im  = 0.0;
-            z_re2 = 0.0;
-            z_im2 = 0.0;
+            iter = 0;
 
-            while((z_re2+z_im2 < MANDELBROT_THRESHOLD) && iter++ < MAX_ITERS)
-            {
-                z_re2 = z_re * z_re;
-                z_im2 = z_im * z_im;
-                z_im = (2.0 * z_re * z_im) + c_im;
-                z_re = z_re2 - z_im2 + c_re;
+            while(0){
+                // mutate
             }
-            c_re += inc_re;
-
-            // write to file a triplet of colors (r,g,b)
-            // apply color scheme indexing/histogramming here
+            c_re += inc;
             ofs << iter << iter << iter;
         }
-        c_im += inc_im; // update the c_im component
-        c_re = init_re; // reset the c_real component
+        c_re = settings->init_real;
+        c_im += inc;
     }
 
-    if(verbose)
-        std::cout << "Finished! Image stored in mandel.ppm" << std::endl;
-
-    // close the file
     ofs.close();
+    if(settings->verbose)
+        std::cout << "Finished writing image" << std::endl;
+    return 0;
+}
+
+
+int main(int argc, char **argv)
+{
+    std::cout << "Breaking program" << std::endl;
+    Settings* render_settings;
+    render_settings = get_render_settings(argc, argv);
+    std::cout << "Passing to render stage" << std::endl;
+
+    render(render_settings);
 
     // exit
     return 0;
---
+++ src/utils.h
@@ -0,0 +1,193 @@
+/*
+ * This file is used to define generic utilities for the project
+ *
+ * Contains: 
+ *   program version constant
+ *   program options and their help messages
+ *   render settings struct and other stuffs surrounding it
+ *   ascii art!
+ */
+
+#ifndef _MANDEL_UTILS_H
+#define _MANDEL_UTILS_H
+
+#include <iostream>
+#include <stdlib.h>
+#include <getopt.h>
+
+#define PROGRAM_VERSION        0.1
+#define RESOLUTION_COUNT        13
+#define ASCII_LINES              9
+#define DEFAULT_ZOOM           1.0
+#define DEFAULT_RE             0.0
+#define DEFAULT_IM             0.0
+
+//////////////////////////////////////////////////////// BEGIN RESOLUTION DATA SECTION
+typedef struct reso_t {
+    const char*  name;
+    const unsigned int width;
+    const unsigned int height;
+} reso_t;
+
+static reso_t all_resolutions[RESOLUTION_COUNT] = {
+    // standard resolution outputs
+    {"480p",       640,    480},
+    {"720p",      1280,    720},
+    {"768p",      1024,    768},
+    {"900p",      1600,    900},
+    {"1080p",     1920,   1080},
+    {"1440p",     2560,   1440},
+
+    // highly deadly resolutions
+    {"4k",        3840,   2160},
+    {"8k",        7680,   4320},
+    {"16k",      15360,   8640},  // ~400mb
+    {"32k",      30720,  17280},  // ~1.6gb !!!!
+    {"64k",      61440,  34560},  // ~6gb, god help us all
+
+    // unconventional rendering resolutions 
+    {"twitter",   1024,    576},
+    {"instagram", 1024,    768},
+
+};
+/////////////////////////////////////////////////////// END RESOLUTION DATA SECTION
+
+
+/////////////////////////////////////////////////////// BEGIN HELP INFO SECTION
+// print out a really terrible hand-made Mandelbrot set
+static const char* ascii_art[ASCII_LINES] = {
+    "------------------*------",
+    "---------------******----",
+    "--------*-----********---",
+    "----*--***--**********--",
+    "--*******************----",
+    "----*--***--**********--",
+    "--------*----*********---",
+    "--------------*******----",
+    "------------------*------",
+};
+
+/*
+ * Print out help info for the program
+ */
+void print_info()
+{
+    for(unsigned int i=0; i < ASCII_LINES; i++)
+        std::cout << ascii_art[i] << std::endl;
+
+
+    std::cout << "Mandelbrot program, version: " << PROGRAM_VERSION << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Help: " << std::endl;
+    std::cout << "-s, --size=<string>    set the image render size" << std::endl;
+}
+
+void print_supported_resolutions()
+{
+    std::cout << "Supported resolutions list: " << std::endl << std::endl;
+    std::cout << "idk" << std::endl;
+};
+/////////////////////////////////////////////////////////// END HELP INFO SECTION
+
+
+/////////////////////////////////////////////////////////// BEGIN RENDER INFO SECTION
+// Class to contain render settings
+// all these represent options fetched from the getopt function
+class Settings
+{
+public:
+    unsigned int verbose;
+    unsigned int color_map;
+    double init_real;
+    double init_imag;
+    double zoom;
+    // char* fname;
+    reso_t*  output_res;
+
+    void display_info();
+};
+
+// getopt_long arguments
+static struct option long_options[] = {
+    {"size",    2,    0, 's'},
+    {"real",    2,    0, 'r'},
+    {"imag",    2,    0, 'i'},
+    {"output",  2,    0, 'o'},
+    {"colors",  2,    0, 'c'},
+    {"zoom",    2,    0, 'z'},
+    {"verbose", 0,    0, 'v'},
+    {"help",    0,    0, 'h'},
+    {NULL,      0, NULL,   0}
+};
+
+/*
+ * get the program render settings from getopt_long
+ * and parse it all into a reso_t struct
+ */
+Settings* get_render_settings(int argc, char** argv)
+{
+    int c;
+    int found        = 0;
+    int option_index = 0;
+
+    // values to use in the struct
+    unsigned int verbose      =   0;
+    unsigned int color        =   0;
+    double magnification      = 0.0;
+    double init_real          = 0.0;
+    double init_imag          = 0.0;
+    char*  fname; //strmcpy to this address from the getopt loop
+    reso_t* selected_reso     = &all_resolutions[0];
+    while ((c = getopt_long(argc, argv, "s:r:i:o:c:z:vh",
+                            long_options, &option_index)) != -1)
+        switch(c)
+        {
+            case 'v':
+                verbose = 1;
+                break;
+            case 'h':
+                print_info();
+                exit(0);      // exit here because we can't return-exit
+                break;
+            case 's':
+                if(strlen(optarg) == 0)
+                {
+                    std::cerr << "No size given" << std::endl;
+                    exit(1);
+                }
+                for(unsigned int ri=0; ri < RESOLUTION_COUNT; ri++)
+                {
+                    if(strcmp(all_resolutions[ri].name, optarg) == 0)
+                    {
+                        selected_reso = &all_resolutions[ri];
+                        found = 1;
+                    }
+                }
+                if(!found)
+                {
+                    std::cerr << "Error: given resolution not supported" << std::endl;
+                    exit(2);
+                }
+                break;
+            case 'z':
+                if(strlen(optarg) == 0)
+                {
+                    std::cerr << "poop" << std::endl;
+                    exit(3);
+                }
+                magnification = atof(optarg);
+                if(magnification <= 0.0)
+                {
+                    std::cerr << "Error: negative or invalid zoom given" << std::endl;
+                    exit(4);
+                }
+                break;
+        }
+
+    return new Settings();
+    // done handling getopts
+}
+
+/////////////////////////////////////////////////////////// END RENDER INFO SECTION
+#endif
